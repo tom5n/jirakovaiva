fe85f431f392525161ca4dccc0abc8e39deeeb80
tom5n <tomasdocekal15@gmail.com>
2025-05-31 20:44:14 +0200

    Remove Next.js middleware and update auth to use cookies

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -190,7 +190,8 @@ export default function Admin() {
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('admin_session');
+    // Odstranit cookie při odhlášení
+    document.cookie = "admin_session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
     window.location.href = '/login';
   }
 
